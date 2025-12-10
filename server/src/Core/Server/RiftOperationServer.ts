bcf02d225d1d54d2d388b169b544b1f246513c25
Ethan Carollo <etcarollo@gmail.com>
2025-12-10 20:55:51 +0100

    Refactor : remove stupid useless code lol

--- server/src/Core/Server/RiftOperationServer.ts
+++ server/src/Core/Server/RiftOperationServer.ts
@@ -1,36 +1,27 @@
 import { RawData } from "ws";
 import { Client } from "../../Framework/Server/Client";
-import ServerState from "./State/ServerState";
-import StrangerState from "./State/StrangerState";
 import { Server } from "../../Framework/Server/Server";
 import Logger from "../../Framework/Utils/Logger";
 
 export class RiftOperationServer extends Server {
-  state: ServerState;
-  
-  constructor(port: number) {
+  constructor(port: number, state: string = "stranger") {
     super(port)
-    this.state = new StrangerState(this)
-  }
-
-  swapState(state: ServerState){
-    this.state = state;
   }
   
   onConnection(client: Client): void {
-    this.state.onConnection(client)
+    // Execute logics for onConnection, currently useless
   }
 
   onMessage(client: Client, msg: RawData) {
     try {
       const parsed = JSON.parse(msg.toString());
-      this.state.onMessage(client, parsed);
+      this.broadcast(parsed, client);
     } catch {
       Logger.error("Didn't receive JSON on receive message.")
     }
   }
   
   onClose(client: Client): void {
-    this.state.onClose(client)
+    // Execute logics for onClose, currently useless
   }
 }
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,4 +1,5 @@
 import { RiftOperationServer } from "./Core/Server/RiftOperationServer";
 
 const port = 8080;
+
 new RiftOperationServer(port);
