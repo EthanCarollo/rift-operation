aa4b9ec2b7daa1fde650e85249880e209c9b586b
Ethan Carollo <etcarollo@gmail.com>
2025-12-11 21:16:15 +0100

    Fix : server files useless for deployment

--- server/src/experiences/Experience.ts
+++
@@ -1,5 +0,0 @@
-export interface Experience {
-  start(): void;          // Called when experience becomes active
-  stop(): void;           // Cleanup if needed
-  handleCommand(client: any, cmd: any): void;
-}
