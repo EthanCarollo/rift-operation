9824b7bf2cf5216cd5b6d9146cf45c791aaacfc3
Ethan Carollo <etcarollo@gmail.com>
2025-12-15 13:16:01 +0100

    Fix : import of stranger web socket button delegate

--- iot/esp/src/Core/Stranger/StrangerWebSocketButtonDelegate.py
+++ iot/esp/src/Core/Stranger/StrangerWebSocketButtonDelegate.py
@@ -1,7 +1,7 @@
 from src.Framework.Button.Button import Button, ButtonDelegate
 from src.Framework.EspController import EspController
 
-class StrangersWebSocketButtonDelegate(ButtonDelegate):
+class StrangerWebSocketButtonDelegate(ButtonDelegate):
     def __init__(self, controller: EspController):
         self.controller = controller
 
