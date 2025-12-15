3d15b2fe13e1057e92fd786d9488adedf0f67df8
Tom Boullay <tom@famille-boullay.fr>
2025-12-15 13:34:33 +0100

    Update main.py

--- iot/esp/main.py
+++ iot/esp/main.py
@@ -2,29 +2,28 @@ import gc
 import uasyncio as asyncio
 
 from src.Framework.Config.ConfigFactory import ConfigFactory
-from src.Framework.Button.Button import Button
 
 from src.Core.Controller.StrangerController import StrangerController
 from src.Core.Controller.DepthController import DepthController
-from src.Core.Controller.LostWokshop.LostController import LostController
-from src.Core.Controller.LostWokshop.LostButtonDelegate import LostButtonDelegate
+from src.Core.Controller.LostController import LostController 
 
 gc.collect()
 
 try:
     config = ConfigFactory.create_cudy_config()
+
     controller = StrangerController(config)
     controller = DepthController(config)
-
     controller = LostController(config)
-    button = Button(pin_id=27, delegate=LostButtonDelegate(controller))     
-    
+
     asyncio.run(controller.main())
+
 except KeyboardInterrupt:
     pass
+
 except Exception as e:
     print(e)
-    pass
+
 finally:
-    if 'controller' in locals():
+    if "controller" in locals():
         controller.cleanup()
