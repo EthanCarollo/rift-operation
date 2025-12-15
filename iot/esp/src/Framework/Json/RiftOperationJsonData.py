bea5475a5f9d3846f081abf93643c767fe7bf040
Ethan Carollo <etcarollo@gmail.com>
2025-12-15 14:09:51 +0100

    WIP : rename whole json for anthony

--- iot/esp/src/Framework/Json/RiftOperationJsonData.py
+++ iot/esp/src/Framework/Json/RiftOperationJsonData.py
@@ -7,12 +7,12 @@ class RiftOperationJsonData:
                 start_system=None, 
                 recognized_stranger_name=None, 
                 pinguin_micro=None, pinguin_audio=None,
-                step_1_parent_sucess=None, 
-                step_2_parent_sucess=None, 
-                step_3_parent_sucess=None, 
-                step_1_enfant_sucess=None, 
-                step_2_enfant_sucess=None, 
-                step_3_enfant_sucess=None, 
+                depth_step_1_parent_sucess=None, 
+                depth_step_2_parent_sucess=None, 
+                depth_step_3_parent_sucess=None, 
+                depth_step_1_enfant_sucess=None, 
+                depth_step_2_enfant_sucess=None, 
+                depth_step_3_enfant_sucess=None, 
                 torch_scanned=None, 
                 cage_is_on_monster=None, 
                 preset_stranger=None, 
@@ -25,12 +25,12 @@ class RiftOperationJsonData:
         self.recognized_stranger_name = recognized_stranger_name
         self.pinguin_micro = pinguin_micro
         self.pinguin_audio = pinguin_audio
-        self.step_1_parent_sucess = step_1_parent_sucess
-        self.step_2_parent_sucess = step_2_parent_sucess
-        self.step_3_parent_sucess = step_3_parent_sucess
-        self.step_1_enfant_sucess = step_1_enfant_sucess
-        self.step_2_enfant_sucess = step_2_enfant_sucess
-        self.step_3_enfant_sucess = step_3_enfant_sucess
+        self.depth_step_1_parent_sucess = depth_step_1_parent_sucess
+        self.depth_step_2_parent_sucess = depth_step_2_parent_sucess
+        self.depth_step_3_parent_sucess = depth_step_3_parent_sucess
+        self.depth_step_1_enfant_sucess = depth_step_1_enfant_sucess
+        self.depth_step_2_enfant_sucess = depth_step_2_enfant_sucess
+        self.depth_step_3_enfant_sucess = depth_step_3_enfant_sucess
         self.torch_scanned = torch_scanned
         self.cage_is_on_monster = cage_is_on_monster
         self.preset_stranger = preset_stranger
