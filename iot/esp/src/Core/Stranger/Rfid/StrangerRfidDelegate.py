a6a5765e43a21bdbccfe56c33250d7e4a3b6d892
Ethan Carollo <etcarollo@gmail.com>
2025-12-16 15:35:18 +0100

    Fix : typo in stranger rfid delegate

--- iot/esp/src/Core/Stranger/Rfid/StrangerRfidDelegate.py
+++ iot/esp/src/Core/Stranger/Rfid/StrangerRfidDelegate.py
@@ -2,5 +2,5 @@ from src.Framework.Rfid.RfidDelegate import RFIDDelegate
 
 class StrangerRFIDDelegate(RFIDDelegate) :
     def on_read(self, uid, reader_name):
-        print("Read : " reader_name + " UID : " + uid)
+        print("Read : " + reader_name + " UID : " + uid)
         return super().on_read(uid, reader_name)
