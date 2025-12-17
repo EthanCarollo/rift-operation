3d00563276281c388d560f6af77812096f3978c6
Ethan Carollo <etcarollo@gmail.com>
2025-12-17 11:11:50 +0100

    Chore : update seo data to making the tools more polished

--- tools/nuxt.config.ts
+++ tools/nuxt.config.ts
@@ -6,6 +6,12 @@ export default defineNuxtConfig({
   css: ['./app/assets/css/main.css'],
   app: {
     head: {
+      title: 'Rift Operation Tools',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: 'The tools used by the Rift Operation group for making interactive and immersive experiences.' }
+      ],
       link: [{ rel: 'icon', type: 'image/png', href: '/favicon.png' }]
     }
   }
