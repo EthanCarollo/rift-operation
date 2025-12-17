9b8cf8ac4691ea6b0e9ebb7f4dc1ed99138542ac
Ethan Carollo <etcarollo@gmail.com>
2025-12-17 11:03:10 +0100

    Refactor : add env.sample.py

--- iot/esp/main.py
+++ iot/esp/main.py
@@ -1,30 +1,11 @@
 import gc
 import uasyncio as asyncio
-
-from src.Framework.Config.ConfigFactory import ConfigFactory
-from src.Core.Lost.LostConfig import LostConfigFactory
-from src.Core.Depth.DepthConfig import DepthConfigFactory
-
-from src.Core.Controller.Stranger.StrangerParentController import StrangerParentController
-from src.Core.Controller.TableController import TableController
-from src.Core.Controller.DepthController import DepthController
-from src.Core.Controller.LostController import LostController 
+from env import controller
 
 gc.collect()
 
 try:
-    config = ConfigFactory.create_cudy_stranger_config()
-    # config = LostConfigFactory.create_child()
-    # config = LostConfigFactory.create_parent()
-    # config = DepthConfigFactory.create_default_child()
-
-    # controller = TableController(config)
-    controller = StrangerParentController(config)
-    # controller = DepthController(config)
-    # controller = LostController(config)
-
     asyncio.run(controller.main())
-
 except KeyboardInterrupt:
     pass
 
