819cffc7d8e79c20992852c80cbb9b59cd078bc9
Ethan Carollo <etcarollo@gmail.com>
2025-12-19 19:07:47 +0100

    WIP : add knntest

---
+++ lab/knn/knntest/knntestTests/knntestTests.swift
@@ -0,0 +1,17 @@
+//
+//  knntestTests.swift
+//  knntestTests
+//
+//  Created by eth on 19/12/2025.
+//
+
+import Testing
+@testable import knntest
+
+struct knntestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
