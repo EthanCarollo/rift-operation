887c0cb5f5d28aef8b32c19efec7f4d7910471f8
Ethan Carollo <etcarollo@gmail.com>
2025-12-20 11:41:50 +0100

    Refactor : add dev chips

--- iot/operator/app/app.vue
+++ iot/operator/app/app.vue
@@ -1,4 +1,5 @@
 <template>
+  <div v-if="route.name !== 'monitor'" class="fixed top-0 left-0 bg-red-600 text-white px-3 py-1.5 text-sm font-bold z-50 pointer-events-none">DEV</div>
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
@@ -6,4 +7,5 @@
 
 <script setup lang="ts">
 import '~/assets/css/main.css'
+const route = useRoute()
 </script>
