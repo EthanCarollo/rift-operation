72bc8cd320dbfdf2a8971400817cc7e76982ae92
Ethan Carollo <etcarollo@gmail.com>
2025-12-21 12:03:32 +0100

    Refactor : create swift app for scenography instead of a webpage

---
+++ iot/scenography-monitor/scenography-monitor/scenography_monitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  scenography_monitorApp.swift
+//  scenography-monitor
+//
+//  Created by eth on 21/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct scenography_monitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
