d545fa5b7f6ff3f9562ae9e7afca4473b67aed36
Ethan Carollo <etcarollo@gmail.com>
2025-12-24 16:14:27 +0100

    Style : update icons of different application and wip on lost knn

---
+++ iot/lost-knn/lost-knn/lost_knnApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct lost_knnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iot/lost-knn/lost-knnTests/lost_knnTests.swift
@@ -0,0 +1,17 @@
+//
+//  lost_knnTests.swift
+//  lost-knnTests
+//
+//  Created by eth on 24/12/2025.
+//
+
+import Testing
+@testable import lost_knn
+
+struct lost_knnTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
