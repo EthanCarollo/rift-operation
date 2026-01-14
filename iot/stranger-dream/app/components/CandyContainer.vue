2da450c4f2bfbf96bd11ed119252ac8dff00b1c5
Ethan Carollo <etcarollo@gmail.com>
2026-01-15 00:21:08 +0100

    Refactor : update style of candy container

--- iot/stranger-dream/app/components/CandyContainer.vue
+++ iot/stranger-dream/app/components/CandyContainer.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="relative flex items-start justify-center w-full max-w-4xl mx-auto">
+    <div class="relative flex items-start justify-center w-full max-w-5xl mx-auto">
         <!-- Candy image background -->
         <img src="/images/stranger/question-container-candy.png" alt="" class="relative w-full h-auto block" />
 
         <!-- Content -->
-        <div class="absolute inset-0 z-10 flex flex-col items-center px-8 justify-start text-center">
+        <div class="absolute inset-0 z-10 flex flex-col items-center px-16 h-full justify-start text-center">
             <!-- Main content (centered) -->
-            <div class=" flex-1 flex flex-col items-center justify-start w-full px-48 pb-8 pt-12">
+            <div class=" flex-1 flex flex-col items-center justify-center w-full px-48 pb-16 pt-0">
                 <slot></slot>
             </div>
         </div>
