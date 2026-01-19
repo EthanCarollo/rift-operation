a6f2203352005d7da658e1e9717a4f084361b612
Tom Boullay <tom@famille-boullay.fr>
2026-01-19 19:24:26 +0100

    fix cam

--- iot/operator/nuxt.config.ts
+++ iot/operator/nuxt.config.ts
@@ -1,8 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
+export default {
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
+  runtimeConfig: {
+    public: {
+      // 1. Local WPA
+      wsUrl: 'ws://192.168.10.7:8000/ws',
+
+      // 2. PRODUCTION (Online Server)
+      // wsUrl: 'wss://server.riftoperation.ethan-folio.fr/ws',
+    }
+  },
   app: {
     head: {
       link: [
@@ -10,4 +19,4 @@ export default defineNuxtConfig({
       ]
     }
   }
-})
+}
