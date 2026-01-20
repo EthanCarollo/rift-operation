1f1e82bbb7dba445c36742098d524ff9444c3db9
Ethan Carollo <etcarollo@gmail.com>
2026-01-20 18:06:37 +0100

    Fix : update key for different letter

--- iot/esp/src/Core/Stranger/Rfid/StrangerRfidDelegate.py
+++ iot/esp/src/Core/Stranger/Rfid/StrangerRfidDelegate.py
@@ -1,10 +1,10 @@
 from src.Framework.Rfid.RfidDelegate import RFIDDelegate
 
 class StrangerRFIDDelegate(RFIDDelegate) :
-    P_LETTER = "13-F6-9B-0F-71"
-    A_LETTER = "F3-4B-54-13-FF"
-    U_LETTER = "E3-6A-75-AD-51"
-    L_LETTER = "C3-BF-C0-2F-93"
+    P_LETTER = "9C-A7-BA-2A-AB"
+    A_LETTER = "83-FC-3C-13-50"
+    U_LETTER = "33-A7-83-0E-19"
+    L_LETTER = "F3-45-F6-F4-B4"
 
     UID_MAP = {
         P_LETTER: "P",
@@ -17,6 +17,7 @@ class StrangerRFIDDelegate(RFIDDelegate) :
         self.callback = callback
 
     def on_read(self, uid, reader_name):
+        print(uid)
         letter = self.UID_MAP.get(uid)
         if letter and self.callback:
             self.callback.on_letter_detected(reader_name, letter)
@@ -24,3 +25,4 @@ class StrangerRFIDDelegate(RFIDDelegate) :
     def on_card_lost(self, uid, reader_name):
         if self.callback:
             self.callback.on_letter_lost(reader_name)
+
